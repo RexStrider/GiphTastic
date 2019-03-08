b006ed92b5b146c1766b95a325e448a73601bcc5
Odysseus <arwaterman@gmail.com>
2019-03-07 16:40:14 -0800

    event listener created for clicking on create animal button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-    let search = "cats"
+    let search = "cats";
 
     // queryURL for Giphy API
     let queryURL = "https://api.giphy.com/v1/gifs/search?q="+search+"&api_key="+config.GIPHY_KEY;
@@ -11,4 +11,36 @@
         console.log(myJson);
     });
 
-    
+    (function(window, document, undefined){
+
+        // code that should be taken care of right away
+        
+        window.onload = init;
+        
+        function init(){
+            // the code to be called when the dom has loaded
+            // #document has its nodes
+            let animalBtn = document.getElementById("create-animal-button");
+
+            console.log(animalBtn);
+
+            animalBtn.addEventListener("click", addAnimalSearchButton);
+        }
+        
+    })(window, document, undefined);
+
+    // let animalBtn = document.getElementById("create-animal-button");
+
+    // console.log(animalBtn);
+
+    // animalBtn.addEventListener("click", addAnimalSearchButton);
+
+    function addAnimalSearchButton(event) {
+        event.preventDefault();
+
+        let animal = document.getElementById("add-animal-input");
+
+        console.log(animal);
+        console.log(animal.value);
+        // if (animal.value )
+    }
